30032afb51e0d4e715d662e6e8563a2f3475e272
Bas Broek <bas@basbroek.nl>
2019-07-02 11:54:38 +0200

    Support multiple windows

---
+++ CatchUp/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  CatchUp
+//
+//  Created by Bas Thomas Broek on 02/07/2019.
+//  Copyright © 2019 Bas Thomas Broek. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+}
